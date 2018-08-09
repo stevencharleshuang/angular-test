1d35355f415a159140c8be9662dbc9099abb9ece
Steven Huang <steven.charles.huang@gmail.com>
2018-08-08 23:07:54 -0400

    modified boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-test'`, async(() => {
+  it(`should have as title 'Angular-Test'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.title).toEqual('Angular-Test');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Test!');
   }));
 });
